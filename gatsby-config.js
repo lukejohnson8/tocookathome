0ae0e20df4de6dbefa09ca96c94ad481f1282d2a
Luke <64995350+lukejohnson8@users.noreply.github.com>
2021-05-14 08:14:15 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'To Cook at Home',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Make Saturday your favorite night of the week, even if you don&#39;t have a sitter. Date nights in cooking, are a great way to do just that.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
